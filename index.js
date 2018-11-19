ff8c7af158c79671ecf404b3d93352ea590317ce
matt.mccoy@patientco.com <matt.mccoy@patientco.com>
2018-11-19 16:47:39 -0500

    Updated index.js to move gistId to its own variable at the top of the file

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const homedir = require('os').homedir();
 const GitHubClient = require('./libs/GitHubClient.js').GitHubClient;
 const gists = require('./libs/features/gists');
 const bashprofuri = homedir+"/.bash_profile";
+const gistId = '2d6b4b93894be88894ca8d17b0e8328f';
 
 let githubCli = new GitHubClient({
     baseUri:"https://api.github.com",
@@ -13,7 +14,7 @@ fs.readFile(bashprofuri, 'utf8', function(err, data) {
     if (err) throw err;
     var bashProfContents = data;
 
-    githubCli.editGist({gistId:'2d6b4b93894be88894ca8d17b0e8328f', fileName:'.bash_profile', content:bashProfContents})
+    githubCli.editGist({gistId:gistId, fileName:'.bash_profile', content:bashProfContents})
 	  .then(gist => {
 	    console.log('.bash_profile synced to gist: '+gist.url);
 	  })
